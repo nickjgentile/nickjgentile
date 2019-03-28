e34b577de7bc26b486166b6bff5db0b59c9cbeb1
Nick Gentile <nickjgentile@gmail.com>
2019-03-27 21:08:07 -0500

    Getting routing setup :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+// Handles the Routing of the application
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Holy fuck I'm back in React.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+            <Link to={`/work`}  >Work</Link>
+            <Link to={`/about`}  >About</Link>
+            <Link to={`/contact`}  >Contact</Link>
+            <Link to={`/work/league`}  >League</Link>
+
+            <Route exact path="/" render={() => <div>You're at exactly the /</div>} />
+            {/* <Route path="/work" component={Work} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} /> */}
+        </div>
+      </Router>
     );
   }
 }
