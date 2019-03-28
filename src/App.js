584ba7a9b16cf8747b8656642ceadc72611ee841
Nick Gentile <nickjgentile@gmail.com>
2019-03-27 21:14:41 -0500

    Routing progress :)

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-            <Link to={`/work`}  >Work</Link>
-            <Link to={`/about`}  >About</Link>
-            <Link to={`/contact`}  >Contact</Link>
-            <Link to={`/work/league`}  >League</Link>
+            <Link to={`/`}  >Root</Link>&nbsp;
+            <Link to={`/work`}  >Work</Link>&nbsp;
+            <Link to={`/about`}  >About</Link>&nbsp;
+            <Link to={`/contact`}  >Contact</Link>&nbsp;
+            <Link to={`/work/league`}  >League</Link>&nbsp;
 
-            <Route exact path="/" render={() => <div>You're at exactly the /</div>} />
+            <Route exact path="/" render={() => <div>You're at exactly the /</div> } />
             {/* <Route path="/work" component={Work} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} /> */}
